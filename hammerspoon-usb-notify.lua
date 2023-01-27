2f0332e9dc32a5485ca177a36cd00f96de46642d
Michael Shipley <admin@dotexe.cf>
2023-01-26 19:41:26 -0500

    Update hammerspoon-usb-notify.lua

--- hammerspoon-usb-notify.lua
+++ hammerspoon-usb-notify.lua
@@ -5,7 +5,7 @@ function usbDeviceCallback(data)
         hs.notify.new({title="Device Connected", informativeText=data["productName"] .. " [" .. data["vendorID"] .. ":" .. data["productID"] .. "] has been connected!"}):send()
     elseif (data["eventType"] == "removed") then
         hs.notify.new({title="Device Disconnected", informativeText=data["productName"] .. " [" .. data["vendorID"] .. ":" .. data["productID"] .. "] has been disconnected!"}):send()
-	end
+    end
 end
 
 usbWatcher = hs.usb.watcher.new(usbDeviceCallback)

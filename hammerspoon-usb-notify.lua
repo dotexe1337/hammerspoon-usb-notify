7e710d0afa203e0e9ff18cb1de2da7d6e0076220
Michael Shipley <admin@dotexe.cf>
2023-01-26 19:40:50 -0500

    Update hammerspoon-usb-notify.lua

--- hammerspoon-usb-notify.lua
+++ hammerspoon-usb-notify.lua
@@ -1,9 +1,6 @@
 usbWatcher = nil
 
 function usbDeviceCallback(data)
-    if (data["productName"] == "ScanSnap S1300i") then
-        
-    end
     if (data["eventType"] == "added") then
         hs.notify.new({title="Device Connected", informativeText=data["productName"] .. " [" .. data["vendorID"] .. ":" .. data["productID"] .. "] has been connected!"}):send()
     elseif (data["eventType"] == "removed") then
